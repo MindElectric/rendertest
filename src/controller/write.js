a6457af25311ee6b47a9a73008d507d3d87eeca7
Kevin Reyes <kevin2801@outlook.com>
2023-12-17 04:53:08 -0600

    deleted a folder

--- src/controller/write.js
+++
@@ -1,7 +0,0 @@
-const puppeteer = require('puppeteer');
-
-const searchInput = await page.$('#sb_form_q'); // Replace with the actual selector for Bing's search box
-
-await searchInput.type('Puppetter js');
-
-await page.keyboard.press('Enter');
